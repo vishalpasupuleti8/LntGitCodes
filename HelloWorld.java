63b164db85fe3346b3fe231019d9a89f7a794356
vishalpasupuleti8 <100670222+vishalpasupuleti8@users.noreply.github.com>
2022-03-01 16:29:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,16 @@
 public class HelloWorld{
+	
 public static void main(String [] args){
 	System.out.println("Hello World from Vishal");
-	int a=10;
+   int a=10;
 	System.out.println(a);
 	
-	String s="Hello";
-		int b=20;
-		String s1="Java";
+   String s="Hello";
+	
+	int b=20;
+	
+	int c=50;
+	
+	 String s1="Java";
 }
 }
